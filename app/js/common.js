eae9930ec4706979d8c7a64c4b16861b4c31911d
nikomax <maks.chuprina@gmail.com>
2017-08-04 00:26:20 +0300

    added arrows

--- app/js/common.js
+++ app/js/common.js
@@ -1,12 +1,13 @@
 $('.single-item').slick({
-    dots: true
+        dots: true
 });
 
-var hamburger =document.querySelector(".hamburger");
+
+var hamburger = document.querySelector(".hamburger");
 var hamburgerLines = document.querySelectorAll('.hamburger__line');
 var navMenu = document.querySelector('.nav');
 var bannerContent = document.querySelector('.container--banner');
-hamburger.onclick = function(){
+hamburger.onclick = function () {
     for (var i = 0; i < hamburgerLines.length; i++) {
         hamburgerLines[i].classList.toggle('hamburger__line--active');
     }
