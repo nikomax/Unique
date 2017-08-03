e32a1fd5f0893ab83219513dbf343bcc1837537a
nikomax <maks.chuprina@gmail.com>
2017-08-03 17:06:38 +0300

    header and banner

--- app/js/common.js
+++ app/js/common.js
@@ -1,3 +1,15 @@
-/**
- * Created by maksc on 27.07.2017.
- */
+$('.single-item').slick({
+    dots: true
+});
+
+var hamburger =document.querySelector(".hamburger");
+var hamburgerLines = document.querySelectorAll('.hamburger__line');
+var navMenu = document.querySelector('.nav');
+var bannerContent = document.querySelector('.container--banner');
+hamburger.onclick = function(){
+    for (var i = 0; i < hamburgerLines.length; i++) {
+        hamburgerLines[i].classList.toggle('hamburger__line--active');
+    }
+    navMenu.classList.toggle('nav--mobile');
+    bannerContent.classList.toggle('banner__content--hidden');
+}
--- gulpfile.js
+++ gulpfile.js
@@ -12,8 +12,8 @@ var gulp       = require('gulp'), // Подключаем Gulp
     autoprefixer = require('gulp-autoprefixer');// Подключаем библиотеку для автоматического добавления префиксов
 
 gulp.task('sass', function(){ // Создаем таск Sass
-    return gulp.src('app/scss/*.scss') // Берем источник
-        .pipe(sass()) // Преобразуем Sass в CSS посредством gulp-sass
+    return gulp.src('app/scss/main.scss') // Берем источник
+        .pipe(sass().on('error', sass.logError)) // Преобразуем Sass в CSS посредством gulp-sass
         .pipe(autoprefixer(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], { cascade: true })) // Создаем префиксы
         .pipe(gulp.dest('app/css')) // Выгружаем результата в папку app/css
         .pipe(browserSync.reload({stream: true})) // Обновляем CSS на странице при изменении
@@ -31,7 +31,7 @@ gulp.task('browser-sync', function() { // Создаем таск browser-sync
 gulp.task('scripts', function() {
     return gulp.src([ // Берем все необходимые библиотеки
         'app/libs/jquery/dist/jquery.min.js', // Берем jQuery
-        'app/libs/magnific-popup/dist/jquery.magnific-popup.min.js' // Берем Magnific Popup
+        'app/libs/slick.min.js'
     ])
         .pipe(concat('libs.min.js')) // Собираем их в кучу в новом файле libs.min.js
         .pipe(uglify()) // Сжимаем JS файл
@@ -45,8 +45,8 @@ gulp.task('css-libs', ['sass'], function() {
         .pipe(gulp.dest('app/css')); // Выгружаем в папку app/css
 });
 
-gulp.task('watch', ['browser-sync', 'css-libs', 'scripts'], function() {
-    gulp.watch('app/sass/**/*.sass', ['sass']); // Наблюдение за sass файлами в папке sass
+gulp.task('watch', ['browser-sync', 'css-libs', 'scripts', 'sass'], function() {
+    gulp.watch('app/scss/**/*.scss', ['sass']); // Наблюдение за sass файлами в папке sass
     gulp.watch('app/*.html', browserSync.reload); // Наблюдение за HTML файлами в корне проекта
     gulp.watch('app/js/**/*.js', browserSync.reload);   // Наблюдение за JS файлами в папке js
 });
