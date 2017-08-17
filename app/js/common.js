f58ad409cc1f8b072e258740967385b3dd466d79
nikomax <maks.chuprina@gmail.com>
2017-08-17 17:18:01 +0300

    full project, need to find bugs

--- app/js/common.js
+++ app/js/common.js
@@ -1,25 +1,67 @@
 $('.single-item').slick({
-        dots: true
+        dots: true,
+        arrows:true,
+    responsive: [
+        {
+            breakpoint: 1024,
+            settings: {
+                arrows: true
+            }
+        },
+        {
+            breakpoint: 768,
+            settings: {
+                arrows: false
+            }
+        }
+]
 });
-$('.multiple-items').slick({
-    infinite: true,
+// $('.responsive').slick({
+//     infinite: true,
+//     slidesToShow: 2,
+//     slidesToScroll: 1,
+//     arrows: false,
+//     dots: true
+// });
+$('.responsive').slick({
+    dots: true,
+    infinite: false,
+    speed: 300,
+    arrows: false,
     slidesToShow: 2,
     slidesToScroll: 1,
-    arrows: false,
-    dots: true
+    responsive: [
+        {
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1,
+                infinite: true,
+                dots: true
+            }
+        },
+        {
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                infinite: true,
+                dots: true
+            }
+        }
+
+    ]
 });
 
-// $(document).ready(function(){
-//     if ($(window).width() <= '768'){
-//         $('.single-item').slick({
-//             arrows: false
-//         });
-//     } else {
-//         $('.single-item').slick({
-//             arrows: true
-//         });
+// var w = $(window).width();
+//     $('.multiple-items').slick({if (w < '768'){
+//         slidesToShow: 2;
+//     } else if ($(window).width() >= '768'){
+//         slidesToShow: 2
 //     }
-// });
+//     });
+
+
 
 
 var hamburger = document.querySelector(".hamburger");
