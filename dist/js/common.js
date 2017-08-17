32d763cae564128c39187e5ba1e509f50bb46c1b
nikomax <maks.chuprina@gmail.com>
2017-08-17 18:34:35 +0300

    build

---
+++ dist/js/common.js
@@ -0,0 +1,59 @@
+$('.single-item').slick({
+        dots: true,
+        arrows:true,
+    responsive: [
+        {
+            breakpoint: 1024,
+            settings: {
+                arrows: true
+            }
+        },
+        {
+            breakpoint: 768,
+            settings: {
+                arrows: false
+            }
+        }
+]
+});
+$('.responsive').slick({
+    dots: true,
+    infinite: false,
+    speed: 300,
+    arrows: false,
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    responsive: [
+        {
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1,
+                infinite: true,
+                dots: true
+            }
+        },
+        {
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                infinite: true,
+                dots: true
+            }
+        }
+
+    ]
+});
+
+var hamburger = document.querySelector(".hamburger");
+var hamburgerLines = document.querySelectorAll('.hamburger__line');
+var navMenu = document.querySelector('.nav');
+var bannerContent = document.querySelector('.container--banner');
+hamburger.onclick = function () {
+    for (var i = 0; i < hamburgerLines.length; i++) {
+        hamburgerLines[i].classList.toggle('hamburger__line--active');
+    }
+    navMenu.classList.toggle('nav--mobile');
+    bannerContent.classList.toggle('banner__content--hidden');
+}
